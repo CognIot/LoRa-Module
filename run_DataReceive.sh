845959fbe81801e2ebbcb91704dc7d9b14f45c5c
Mattb1969 <matthew@bostintechnology.com>
2018-01-04 08:45:50 +0000

    Added script for receiver to use and fixed typo in transfer script

--- run_DataReceive.sh
+++ run_DataReceive.sh
@@ -2,11 +2,11 @@
 
 # Shell script to run LoRa Data Transer application on bootup
 
-#As a NODE
+#As a HUB
 
 cd /home/pi/Projects/LoRa-Module
 
-sudo ./DataTransfer.py --node &
+sudo ./DataTransfer.py --hub &
 
 exit 0
 
