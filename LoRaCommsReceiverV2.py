fbaa04d76b61aded2b339dae582ade0e797f1c62
Matthew Bennett <mattb1969@users.noreply.github.com>
2018-07-21 09:56:39 +0100

    added protection for incorrect response from LoRa module length response
    should have received xx\r\nOK00>, got 0\x00\r\nOK00>

--- LoRaCommsReceiverV2.py
+++ LoRaCommsReceiverV2.py
@@ -320,7 +320,10 @@ class LoRaComms:
             # Expect to get 'xx\r\nOK00>' where xx is the length byte
             reply = self._read_from_sp(9)
             if self._check_lora_response(reply):
-                length = int(reply[0:2], 16)
+                try:
+                    length = int(reply[0:2], 16)
+                except:
+                    length = 0
         logging.info("[LCR]: Sent %s, expected message is %s bytes" %(REC_LEN, length))
         return length
 
